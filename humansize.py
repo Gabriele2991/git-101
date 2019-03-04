cb1e7b3d0596eddcf9f2a7e6a1b31db89d576a5e
Gabriele Mangiavacchi <ninroot90@gmail.com>
2019-03-04 12:57:01 +0100

    branche delete

--- humansize.py
+++ humansize.py
@@ -4,7 +4,7 @@ SUFFIXES = ['KB', 'MB', 'GB', 'TB','PB', 'EB', 'ZB', 'YB']
 def approximate_size(size):
     """COnvert a file size to a human-readable form
     Keywords argument:
-    size -- file size iin bytes
+    size -- file size in bytes
 
     Return:string
     """
